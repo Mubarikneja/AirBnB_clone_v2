3c5228e9e11d3196cc76f6a440c89653066aae0d
Mubarik Neja <mubarikneja@gmail.com>
2023-02-06 15:33:53 +0300

    modif

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,15 +3,18 @@
 from flask import Flask
 """class Flask"""
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """displays 
+    """displays text
     Returns:
-        Hello HBNB!
+        text
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
